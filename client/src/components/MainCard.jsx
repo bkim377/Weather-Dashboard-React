a3133f288aeb51408344cca6b82f206d9cb2f10e
Brandon Kim <bkim377@gatech.edu>
2020-06-10 21:12:53 -0400

    added moment to all cards

--- client/src/components/MainCard.jsx
+++ client/src/components/MainCard.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import moment from "moment";
+import Moment from "react-moment";
 import "./MainCard.css";
 // import API from "../utils/API.jsx";
 
@@ -20,31 +21,31 @@ class MainCard extends React.Component {
                 <h3 className="mx-3 py-2 border border-primary" id="forecast-header">5-Day Forecast:</h3>
                 <div className="row">
                     <div className="card mx-3 my-2" id="day-1">
-                        <h4 className="card-title">06/11/2020</h4>
+                        <h4 className="card-title">{moment().add(1, "days").format("L")}</h4>
                         <img className="mx-auto" id="future-icons-1" alt="1 day ahead"></img>
                         <p className="card-text" id="temp-1">Temp: ___°F</p>
                         <p className="card-text" id="humid-1">Humidity: ___</p>
                     </div>
                     <div className="card mx-3 my-2" id="day-2">
-                        <h4 className="card-title">06/12/2020</h4>
+                        <h4 className="card-title">{moment().add(2, "days").format("L")}</h4>
                         <img className="mx-auto" id="future-icons-2" alt="2 days ahead"></img>
                         <p className="card-text" id="temp-2">Temp: ___°F</p>
                         <p className="card-text" id="humid-2">Humidity: ___</p>
                     </div>
                     <div className="card mx-3 my-2" id="day-3">
-                        <h4 className="card-title">06/13/2020</h4>
+                        <h4 className="card-title">{moment().add(3, "days").format("L")}</h4>
                         <img className="mx-auto" id="future-icons-3" alt="3 days ahead"></img>
                         <p className="card-text" id="temp-3">Temp: ___°F</p>
                         <p className="card-text" id="humid-3">Humidity: ___</p>
                     </div>
                     <div className="card mx-3 my-2" id="day-4">
-                        <h4 className="card-title">06/14/2020</h4>
+                        <h4 className="card-title">{moment().add(4, "days").format("L")}</h4>
                         <img className="mx-auto" id="future-icons-4" alt="4 days ahead"></img>
                         <p className="card-text" id="temp-4">Temp: ___°F</p>
                         <p className="card-text" id="humid-4">Humidity: ___</p>
                     </div>
                     <div className="card mx-3 my-2" id="day-5">
-                        <h4 className="card-title">06/15/2020</h4>
+                        <h4 className="card-title">{moment().add(5, "days").format("L")}</h4>
                         <img className="mx-auto" id="future-icons-5" alt="5 days ahead"></img>
                         <p className="card-text" id="temp-5">Temp: ___°F</p>
                         <p className="card-text" id="humid-5">Humidity: ___</p>
